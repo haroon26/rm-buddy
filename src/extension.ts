b428fd83cdb8638b7efea088f43284842217852f
Haroon Rasheed <haroonar32@gmail.com>
2025-03-16 23:20:23 +0500

    dummy webview added

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,13 @@
 import * as vscode from "vscode";
+import IssuesWebviewViewProvider from "./providers/issues";
 
-export function activate(context: vscode.ExtensionContext) {}
+export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider(
+      "issues",
+      new IssuesWebviewViewProvider(context)
+    )
+  );
+}
 
 export function deactivate() {}
